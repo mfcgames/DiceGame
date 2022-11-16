8b18d3231b7509b75adf5fe87b45f5b7eb2ef124
Muhammed <77715853+mfcgames@users.noreply.github.com>
2022-11-16 19:42:27 +0300

    Update index.js

--- Dicee/index.js
+++ Dicee/index.js
@@ -1,11 +1,14 @@
-var randomNumber=Math.floor(Math.random()*6)+1;
-var randomNumber2=Math.floor(Math.random()*6)+1;
-var image1="images/dice"+randomNumber2+".png";
-var image2 ="images/dice"+randomNumber+".png";
+var randomNumber=Math.floor(Math.random()*6)+1;//Randomization number
+var randomNumber2=Math.floor(Math.random()*6)+1;//Randomization second number
 
+var image1="images/dice"+randomNumber2+".png";//inserting random number to images
+var image2 ="images/dice"+randomNumber+".png";//inserting random number to images
+
+//Inserting images on screen  
 document.querySelector(".img1").setAttribute("src",image1);
 document.querySelector(".img2").setAttribute("src",image2);
 
+//Between two image statements which is equalty, greater than or smaller than other image  
 if (image1 === image2) {
   document.querySelector("h1").textContent ="Draw";
 }else if (image1 > image2) {
